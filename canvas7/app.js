985d0f6bd3e2ccc5d773c1cc5cb2f7e2877c58e8
Forka137 <forka_all@hotmail.com>
2021-01-05 06:15:20 -0300

    canvas7 nueva función
    ahora se pueden mover los largos de ambos eslabones, además se informa el error si es que las dimensiones no son correctas

--- canvas7/app.js
+++ canvas7/app.js
@@ -3,10 +3,15 @@ var canvas = document.getElementById('canvas'),
 	offscreenCanvas = document.createElement('canvas'),
 	offscreenContext = offscreenCanvas.getContext('2d'),
 	speedSlider = document.getElementById('speedSlider'),
+	r2Slider = document.getElementById('r2Slider'),
+	r3Slider = document.getElementById('r3Slider'),
 	speedOutput = document.getElementById('speedOutput'),
+	r2Output = document.getElementById('r2Output'),
+	r3Output = document.getElementById('r3Output'),
 
 	speed = 1.0,
 	paused = true,
+	lengthError = false,
 	lastTime = 0,
 	fps = 0,
 	animateButton = document.getElementById("animateButton"),
@@ -43,6 +48,12 @@ function updatePoints() {
 
 function evaluateVariables(){
 	theta3 = Math.asin((r4 - r2*Math.sin(theta2))/r3);
+
+	if (r2 + r4 > r3) {
+		paused = true;
+		animateButton.value = 'Animar';
+		drawError();
+	}
 }
 
 function calculateFps() {
@@ -74,11 +85,11 @@ function drawPistonBase() {
 	context.shadowOffsetY = 2;
 	context.shadowBlur = 4;
 	context.lineWidth = 2;
-	context.rect(startX - r2 + r3, startY - r4 + rectH/2 + 2, 350, r4 - rectH/2);
+	context.rect(startX - r2 + r3-50, startY - r4 + rectH/2 + 2, r2+r3, r4 - rectH/2);
 	context.lineWidth = 2;
 	context.stroke();
 	context.fillStyle = 'linen';
-	context.fillRect(startX - r2 + r3, startY - r4 + rectH/2 + 2, 350, r4 - rectH/2);
+	context.fillRect(startX - r2 + r3-50, startY - r4 + rectH/2 + 2, r2+r3, r4 - rectH/2);
 	context.restore();
 }
 
@@ -106,14 +117,28 @@ function drawjoints() {
 	context.restore();
 }
 
-/*
+function drawError() {
+	context.save();
+	context.font = '32px Helvetica';
+	context.fillStyle = 'red';
+	context.textAlign = 'center';
+	context.fillText("Largos Incompatibles", canvas.width/2, 100+canvas.height/2);
+	context.fillStyle = 'black';
+	context.fillText("R2 + base > R3", canvas.width/2, 150+canvas.height/2);
+	context.fillText(`(${r2} + ${r4} > ${r3})`, canvas.width/2, 200+canvas.height/2);
+	context.font = '22px Helvetica';
+	context.fillText(`(${r2+r4} > ${r3})`, canvas.width/2, 230+canvas.height/2);
+	context.restore();
+}
+
+
 function drawFps() {
 	context.save();
 	context.fillStyle = 'cornflowerblue';
 	context.fillText(fps.toFixed() + ' fps', 20, 60);
 	context.restore();
 }
-*/
+
 //no me gustó que se actualice tan rápido, 
 //mejor la función setInterval para obtener su valor.
 function updateFps() {
@@ -122,12 +147,14 @@ function updateFps() {
 	} else {
 		frameRate.innerText = "0 fps";
 	}
-	
+	console.log(r2+r3);
 }
 
 function updateText () {
 	text = parseFloat(speed).toFixed(1);
 	speedOutput.innerText = text;
+	r2Output.innerText = r2;
+	r3Output.innerText = r3;
 }
 
 function windowToCanvas(x, y) { 
@@ -158,6 +185,22 @@ function animate(time) {
 	}
 }
 
+function updateFrame() {
+ //actualiza el dibujo en 1 frame
+context.fillStyle = 'rgba(230,230,230,1.0)'
+context.fillRect(0,0, canvas.width, canvas.height);
+ evaluateVariables();
+	points[0] = new Point(startX, startY);
+	points[1] = new Point(startX + r2*Math.cos(theta2),
+						  startY - r2*Math.sin(theta2));
+	points[2] = new Point(startX + r2*Math.cos(theta2) + r3*Math.cos(theta3),
+						  startY - r2*Math.sin(theta2) - r3*Math.sin(theta3));
+drawPistonBase();
+drawPiston();
+drawLines();
+drawjoints();
+}
+
 //event handler
 
 animateButton.onclick = function (e) {
@@ -171,9 +214,25 @@ animateButton.onclick = function (e) {
 }
 
 speedSlider.oninput = function(e) {
-	speed = e.target.value;
+	speed = parseFloat(e.target.value);
 	updateText();
 }
+
+r2Slider.oninput = function(e) {
+	r2 = parseInt(e.target.value);
+	updateText();
+	if (paused) {
+	updateFrame();
+	}
+}
+r3Slider.oninput = function(e) {
+	r3 = parseInt(e.target.value);
+	updateText();
+	if (paused) {
+	updateFrame();
+	}
+}
+
 // requestAnimationFrame() deja al navegador elegir el Frame Rate.
 // esta función se supone que se ajusta al refresco de la pantalla (si es que puede)
 // si se quieren fps variables se debe recurrir a otros métodos
