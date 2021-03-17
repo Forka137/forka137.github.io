f036c682c9dd20ae92a048bb3fb42c285aee4ba3
Forka137 <forka_all@hotmail.com>
2021-03-17 11:26:15 -0300

    Aspecto visual
    Se hizo más bonito el aspecto de los elementos clickeables

--- ufro_en_bloques/acerca_de/app.js
+++ ufro_en_bloques/acerca_de/app.js
@@ -1,3 +1,15 @@
+function lightOn(element) {
+	//console.log("se enciende el div");
+	let w = window.innerWidth;
+	if (w < 1024) element.style.backgroundColor = '#00396E';
+}
+
+function lightOff(element) {
+	//console.log("se apaga el div");
+	let w = window.innerWidth;
+	if (w < 1024) element.style.backgroundColor = '#00407B';
+}
+
 const navSlide = () => {
 	const burger = document.querySelector('.burger-button');
 	const nav = document.querySelector('.nav-links');
@@ -14,9 +26,18 @@ const navSlide = () => {
 			link.style.animation = `navLinkFade 0.5s ease forwards ${index/2}s`;
 		}
 	});
+
+	navLinks.forEach((link) => {
+		link.addEventListener("mouseenter", (event) => {
+		lightOn(link);
+		});
+
+	link.addEventListener("mouseleave", (event) => {
+		lightOff(link);
+		});
+	});
 });
 		
-	
 }
 
 navSlide();
