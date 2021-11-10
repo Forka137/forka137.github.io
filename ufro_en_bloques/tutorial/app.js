18e5e65d06aaf983a75bc7ce8b339f082727a197
Forka137 <forka_all@hotmail.com>
2021-11-10 06:35:40 -0300

    ufro en bloques update
    Aviso y nuevas fotos en la página

--- ufro_en_bloques/tutorial/app.js
+++ ufro_en_bloques/tutorial/app.js
@@ -1,12 +1,12 @@
 function lightOn(element) {
-	//console.log("se enciende el div");
-	let w = window.innerWidth;
+	// console.log("se enciende el div");
+	const w = window.innerWidth;
 	if (w < 1024) element.style.backgroundColor = '#00396E';
 }
 
 function lightOff(element) {
-	//console.log("se apaga el div");
-	let w = window.innerWidth;
+	// console.log("se apaga el div");
+	const w = window.innerWidth;
 	if (w < 1024) element.style.backgroundColor = '#00407B';
 }
 
@@ -15,29 +15,27 @@ const navSlide = () => {
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
 
-	burger.addEventListener('click', () =>{
+	burger.addEventListener('click', () => {
 		nav.classList.toggle('nav-active');
-		//alterna el valor de la clase
-		//animación sin librería, solo usando css
-	navLinks.forEach((link, index) =>{
-		if (link.style.animation) {
-			link.style.animation = '';
-		} else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index/2}s`;
-		}
+		// alterna el valor de la clase
+		// animación sin librería, solo usando css
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			}
+			else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 2}s`;
+			}
+		});
 	});
-
 	navLinks.forEach((link) => {
-		link.addEventListener("mouseenter", (event) => {
-		lightOn(link);
+		link.addEventListener('mouseenter', () => {
+			lightOn(link);
 		});
-
-	link.addEventListener("mouseleave", (event) => {
-		lightOff(link);
+		link.addEventListener('mouseleave', () => {
+			lightOff(link);
 		});
 	});
-});
-		
-}
+};
 
 navSlide();
